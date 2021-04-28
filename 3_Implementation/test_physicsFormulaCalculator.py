84723209dfbe4ee9134f8a9b2913f330927bf7e9
sirisatwika <sirisatwikakotha29@gmail.com>
2021-04-28 11:35:42 +0530

    main

---
+++ 3_Implementation/test_physicsFormulaCalculator.py
@@ -0,0 +1,7 @@
+import physicsFormulaCalculator    # The code to test
+def test_capacitance(charge,voltage):
+    assert inc_dec.increment(4,2) == 2
+#def test_():
+ #   assert inc_dec.decrement(3) == 4
+if __name__ == "__main__":
+    physicsFormulaCalculator.main()
